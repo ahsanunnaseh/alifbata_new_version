d18e636ef959828da975c70ce22cfc72ffd56359
Fauziah Ifa Hasan <Fauziah Ifa Hasan@fauziah>
2015-04-01 00:06:40 +0700

    Testing Form with image

--- alifbata_new_version/src/com/views/NewClass.java
+++ alifbata_new_version/src/com/views/NewClass.java
@@ -5,17 +5,46 @@
  */
 package com.views;
 
+import java.util.Arrays;
+
 /**
  *
  * @author Fauziah Ifa Hasan
  */
 public class NewClass {
 
-    public int isPowerOfTwodd(int x) {
+    public void isPowerOfTwodd(int x) {
         int[] two = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768,
             65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608,
             16777216, 33554432, 67108864, 134217728, 268435456, 536870912};
-        return 1;
+              int [] atas = new int[30];
+            int [] bawah = new int[39];
+        for(int i=0; i<two.length; i++){
+      
+            if(x>two[i]){
+                atas[i]=two[i];
+            }else{
+              bawah[i]= two[i]; 
+            }
+        }
+        System.out.println(Arrays.toString(atas));
+        System.out.println(Arrays.toString(bawah));
+        System.out.println("mendekati : " + getMaxValue(atas));
+
+    }
+    public static int getMaxValue(int[] array){  
+      int maxValue = array[0];  
+      for(int i=1;i < array.length;i++){  
+      if(array[i] > maxValue){  
+      maxValue = array[i];  
+
+         }  
+     }  
+             return maxValue;  
+}  
+    public static void main(String [] args){
+        NewClass nn=new NewClass();
+        nn.isPowerOfTwodd(10000);
     }
 
   
